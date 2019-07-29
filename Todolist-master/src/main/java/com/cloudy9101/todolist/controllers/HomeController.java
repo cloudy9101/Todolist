00ef9aadae756171e6082d9e85a43b78b238388a
cloudy9101 <cloudy9101@gmail.com>
2019-07-29 16:33:50 +1200

    Home page controller and list view

--- Todolist-master/src/main/java/com/cloudy9101/todolist/controllers/HomeController.java
+++ Todolist-master/src/main/java/com/cloudy9101/todolist/controllers/HomeController.java
@@ -2,17 +2,38 @@ package com.cloudy9101.todolist.controllers;
 
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cloudy9101.todolist.models.List;
+import com.cloudy9101.todolist.models.ListRepository;
+import com.cloudy9101.todolist.models.Todo;
+import com.cloudy9101.todolist.models.TodoRepository;
+
 @Controller
 public class HomeController {
+	@Autowired
+	private ListRepository listRepository;
+	@Autowired
+	private TodoRepository todoRepository;
+	
 	@RequestMapping(value = "/home")
-	public ModelAndView home() {
-		HashMap<String, String> params = new HashMap<String, String>();
-        params.put("title", "Hello");
+	public ModelAndView home(Model model) {
+		Iterable<List> lists = listRepository.findAll();
+		HashMap<Integer, Iterable<Todo>> listTodos = new HashMap<Integer, Iterable<Todo>>();
+		lists.forEach(list -> {
+			Iterable<Todo> todos = todoRepository.findByListId(list.getId());
+			listTodos.put(list.getId(), todos);
+		});
+		
+		ModelAndView mav = new ModelAndView();
+	    mav.addObject("lists", lists);
+	    mav.addObject("listTodos", listTodos);
+	    mav.setViewName("home");
 
-        return new ModelAndView("home", params);
+        return mav;
 	}
 }
